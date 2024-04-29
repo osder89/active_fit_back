12879cb1606daf643023df0a7213b44e4fd73bdc
Osder89 <104263852+osder89@users.noreply.github.com>
2024-04-29 14:34:40 -0400

    creacion del proyecto

---
+++ src/main/java/com/example/active_fit_back/ActiveFitBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.active_fit_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveFitBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveFitBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/active_fit_back/ActiveFitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.active_fit_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveFitBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
